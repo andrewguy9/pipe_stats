bb9ae8d594d7eaf66b2823feacbbd7684ea18fed
Andrew Thomson <andrewguy9@athomsonmac.gateway.2wire.net>
2011-11-25 03:16:11 -0800

    Refactored histogram printing.

--- StatCalculator.cpp
+++ StatCalculator.cpp
@@ -33,14 +33,36 @@ int compare_size_t(const void *va, const void *vb) {
 	}
 }
 
+void printHisogram(size_t *data, size_t sampleSize) {
+	qsort(data, sampleSize, sizeof(size_t), compare_size_t);
+	
+	int numPercentiles = 15;
+	int percentiles[] = {10,20,30,40,50,60,70,80,90,95,96,97,98,99,100};
+
+	size_t max = data[sampleSize - 1];
+	for(size_t cur = 0; cur < numPercentiles; cur++) {
+		size_t index = (percentiles[cur] * sampleSize) / 100;
+		if(index >= sampleSize) {
+			index = sampleSize - 1;
+		}
+		size_t num = data[index];
+		std::cout << "\t" << std::setw(3) << percentiles[cur] << "% ";
+		for(int star = 0; star < (num * 60) / max; star++) {
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "Sample Dump Done" << std::endl;
+}
+
 int main(int argc, char ** argv) {
 
 	initMyRand();
 
 	RunningAverage avg;
 	RunningMax max;
-	const size_t sampleLength = 150;
-	ReservoirSample<size_t> sample(sampleLength, myRand);
+	const size_t sampleSize = 1000;
+	ReservoirSample<size_t> sample(sampleSize, myRand);
 
 	do {
 		double val;
@@ -62,24 +84,6 @@ int main(int argc, char ** argv) {
 	std::cout << "Avg is " << avg.Get() << std::endl;
 	std::cout << "Max is " << max.Get() << std::endl;
 
-	size_t *data = sample.GetData();
-	qsort(data, sampleLength, sizeof(size_t), compare_size_t);
-	std::cout << "Sample Dump " << std::endl;
-	int percentiles[] = {10,20,30,40,50,60,70,80,90,95,96,97,98,99,100};
-	int numPercentiles = 15;
-	for(size_t percentile = 0; percentile < numPercentiles; percentile++) {
-		size_t index = (percentiles[percentile] * sampleLength) / 100;
-		if(index >= sampleLength) {
-			index = sampleLength - 1;
-		}
-		size_t num = data[index];
-		std::cout << "\t" << std::setw(3) << percentiles[percentile] << "% ";
-		for(int star = 0; star < (num * 60) / max.Get(); star++) {
-			std::cout << "*";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << "Sample Dump Done" << std::endl;
-
+	printHisogram(sample.GetData(), sampleSize);
 	return 0;
 }
