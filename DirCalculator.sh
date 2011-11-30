49686e52c60c3653218cec2bd193a44e02b2fb84
Andrew Thomson <andrewguy9@athomsonmac.local>
2011-11-30 09:58:17 -0800

    Added script to aggregate directory statistics.

---
+++ DirCalculator.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+find . | 
+xargs -I {} -P1 ls -l {} | 
+grep -v total | 
+awk '{print $5,$9}' | 
+awk '
+{
+	size = $1; 
+    if(match($2, /\.[^\/.]*$/ ) != 0) {
+		ext = substr($2,RSTART,RLENGTH)
+	}else{
+	ext = "no_ext";
+	}
+	print size,ext;
+}' |
+./StatCalculator.out 
