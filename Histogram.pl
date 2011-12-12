147ac58bee714493badf941a44406f189bbd3387
Andrew Thomson <andrewguy9@athomsonmac.local>
2011-12-11 18:31:14 -0800

    Add percentage option to histogram.pl

--- Histogram.pl
+++ Histogram.pl
@@ -8,11 +8,13 @@ use Getopt::Long;
 my $line_width;
 my $bucket_width;
 my $star_width;
+my $use_percentage = 0;
 
 # Collect Options
 GetOptions(
 	"width=i" => \$line_width,
 	"bwidth=i" => \$bucket_width,
+	"percentage" => \$use_percentage,
 ) or die "Invalid input: $!";
 
 # Sanity Check
@@ -25,6 +27,7 @@ my @buckets;
 my @values;
 my $bucket_width_max = 0;
 my $n = 0;
+my $sum = 0;
 while(<>) {
 	chomp;
 
@@ -35,6 +38,7 @@ while(<>) {
 
 	$bucket_width_max = max($bucket_width_max, length($bucket));
 	$n++;
+	$sum += $value;
 }
 
 # Compute Results
@@ -55,7 +59,13 @@ while(@buckets) {
 
 	$bucket = substr $bucket, 0, $bucket_width;
 	$bucket .= " " x ($bucket_width - length($bucket));
-	my $stars = "*" x int($star_width * ($value / $max_value));
+	my $meter_percentage;
+	if($use_percentage) {
+		$meter_percentage = $value / $sum;
+	} else {
+		$meter_percentage = $value / $max_value;
+	}
+	my $stars = "*" x (int($star_width * $meter_percentage));
 
 	printf("%s %s\n", $bucket, $stars);
 }
