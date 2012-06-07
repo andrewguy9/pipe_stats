0a0679f34ff70d9ae756e7695b340878e086a139
Andrew Thomson <athomsonguy@gmail.com>
2012-06-07 16:42:30 -0700

    Make EnterpySourceTest work harder.
    also cleaned up output.

--- EnterpySourceTest.cpp
+++ EnterpySourceTest.cpp
@@ -5,13 +5,13 @@
 #include "EnterpySource.hpp"
 #include "HeavyHitters.hpp"
 
-int main() 
+int main()
 {
         EnterpySource & es = GetEnterpySource();
         HeavyHittersTracker hht(100);
 
 
-        for(size_t i = 0; i < 1000000; i++) {
+        for(size_t i = 0; i < 5000000; i++) {
                 int num = es.rand(1,100);
 
                 std::stringstream stream;
@@ -22,11 +22,9 @@ int main()
                 hht.Update(str, 1);
         }
 
-        std::cout << "Dumping " << std::endl;
         std::map<std::string, double>::iterator cur = hht.begin();
         for(cur = hht.begin(); cur != hht.end(); ++cur) {
                 std::cout << "\t" << cur->first << "\t" << cur->second << " " << std::endl;
         }
-        std::cout << "Dumping Done!" << std::endl;
 }
 
